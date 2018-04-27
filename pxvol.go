58882f33b15c30a26ff6b5f9eece299fe50b6f5e
Antoni Sawicki <as@portworx.com>
2018-04-26 17:28:55 -0700

    correct tabs

--- pxvol.go
+++ pxvol.go
@@ -4,7 +4,7 @@
 // https://docs.docker.com/develop/sdk/examples/
 
 package main
- 
+
 import (
     "io/ioutil"
     "os"
@@ -15,10 +15,10 @@ import (
     //"encoding/json"
 
     //"github.com/docker/docker/api/types"
-   	"github.com/docker/docker/client"
-  	"golang.org/x/net/context"    
+    "github.com/docker/docker/client"
+    "golang.org/x/net/context"
 )
- 
+
 func getcdockercid(pid string) string  {
     cgroup, err := os.Open("/proc/" + pid + "/cgroup")
     if err == nil {
@@ -29,24 +29,24 @@ func getcdockercid(pid string) string  {
             f := strings.Split(scanner.Text(), ":")
             // this can be any other cgroup
             if f[1] == "pids" {
-            	d := strings.Split(f[2], "/")
-				// todo k8s
-            	if d[1] == "docker" && len(d[2]) > 0 {
-                	//fmt.Println(">>> DOCKER:", pid, f[1], d[0], d[1])
-                	return d[2]
+                d := strings.Split(f[2], "/")
+                // todo k8s
+                if d[1] == "docker" && len(d[2]) > 0 {
+                    //fmt.Println(">>> DOCKER:", pid, f[1], d[0], d[1])
+                    return d[2]
                 } else {
-                	return "host"
+                    return "host"
                 }
 
             }
         }
     }
-    
-    
-    
+
+
+
     return "host"
 }
- 
+
 func main() {
     var vol string = "888651093940211871"
 //    var vol string = "848783366117993172"
@@ -61,10 +61,10 @@ func main() {
     if err != nil {
         log.Fatal(err)
     }
-        
+
     for _, pid := range pids {
         if pid.IsDir() && pid.Name()[0] >= '1' && pid.Name()[0] <= '9' {
-            
+
             //fmt.Println("/proc/" + pid.Name() + "/mounts")
             mounts, err := os.Open("/proc/" + pid.Name() + "/mounts")
             if err == nil {
@@ -75,16 +75,16 @@ func main() {
                     f := strings.Fields(scanner.Text())
                     //fmt.Println("  ", f[0], f[1])
                     if f[0] == "/dev/pxd/pxd" + vol && ! strings.HasPrefix(f[1], "/var/lib/osd/mounts") {
-						cid := getcdockercid(pid.Name())
-						if cid != "host" {
-	                        fmt.Println("mnt:", pid.Name(), f[1], cid)
-	                        inspect, err := docker.ContainerInspect(context.Background(), cid)
-	                        if err == nil {
+                        cid := getcdockercid(pid.Name())
+                        if cid != "host" {
+                            fmt.Println("mnt:", pid.Name(), f[1], cid)
+                            inspect, err := docker.ContainerInspect(context.Background(), cid)
+                            if err == nil {
 
-	                            fmt.Println("Inspect:", inspect.Mounts[0].Driver)
-	                            
-	                        }
-	                    }
+                                fmt.Println("Inspect:", inspect.Mounts[0].Driver)
+
+                            }
+                        }
                     }
                 }
             }
